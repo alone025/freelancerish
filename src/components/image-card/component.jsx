74b2823ba6ba596e0cd6810bd906a379c8a6f28d
azadov-azamat <azamatazadov112@gmail.com>
2024-06-09 13:54:45 +0500

    image-card component

--- src/components/image-card/component.jsx
+++ src/components/image-card/component.jsx
@@ -3,14 +3,22 @@ import Image from "next/image";
 
 export default function ImageCardComponent({src, title}) {
     return (
-        <div className={''}>
-            <Image
-                src={src}
-                alt={title}
-                className={'lg:w-[262px] h-[332px] object-cover object-center'}
-                width={262}
-                height={332}
-            />
+        <div className="relative overflow-hidden group max-w-xs md:max-w-md">
+            <div className="relative w-full transition-all duration-300 ease-in-out group-hover:h-2/3">
+                <div className="relative h-64 md:h-96">
+                    <Image
+                        src={src}
+                        alt={title}
+                        layout="fill"
+                        objectFit="cover"
+                        className="absolute"
+                    />
+                </div>
+            </div>
+            <div
+                className="flex justify-center h-28 items-center absolute bottom-0 left-0 right-0 bg-white bg-opacity-90 text-center p-2 transition-all duration-300 ease-in-out transform translate-y-full group-hover:translate-y-0">
+                <h3 className="text-lg font-medium">{title}</h3>
+            </div>
         </div>
     );
 }
